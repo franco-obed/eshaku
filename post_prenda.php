1fc62e948669ede5489be9fc87a49ef80989ecb3
franco-obed <franco.obed@icloud.com>
2020-04-22 22:39:52 -0600

    script que permite depositar la info de la prenda, haciendo multiples consultas a la base de datos

--- post_prenda.php
+++ post_prenda.php
@@ -6,12 +6,20 @@ $tipo = $_POST['tipo'];
 $genero = $_POST['genero'];
 $estado = $_POST['estado'];
 $talla = $_POST['talla'];
-//$tipo = $_POST['tipo'];
-//$tipo = $_POST['tipo'];
-$queryPrenda = "INSERT INTO prenda (tipo, estado, talla, genero) VALUES ('$tipo', '$estado', '$talla', '$genero');";
+$query4Id = mysqli_query($conexion, "SELECT id_usuario FROM usuario WHERE correo = '$e_mail';");
+$arrayUserId = mysqli_fetch_assoc($query4Id);
+$userId = $arrayUserId['id_usuario'];
+$postTitulo = $_SESSION['titulo'];
+$postDescr = $_SESSION['descr'];
+
+
+$queryPrenda = "UPDATE prenda SET tipo = '$tipo', estado = '$estado', talla = '$talla', genero = '$genero', fecha = NOW() WHERE id_usuario = '$userId' AND titulo = '$postTitulo' AND descripcion = '$postDescr';";
 $store_data = mysqli_query($conexion, $queryPrenda);
+$postIdQuery = mysqli_query($conexion, "SELECT id FROM prenda WHERE id_usuario = '$userId' AND titulo = '$postTitulo' AND descripcion = '$postDescr';");
+$lastPostId = mysqli_fetch_assoc($postIdQuery);
+$_SESSION['last_post'] = $lastPostId['id'];
   			echo "<script type='text/javascript'>
 			alert('Se ha subido exitosamente');
+			window.location.href='post.php';
 			</script>";
-			header("location: post.php");
 ?>
